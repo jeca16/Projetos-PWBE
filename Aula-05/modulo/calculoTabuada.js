15373dca22d327ca6e331c418819dee413421f97
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2023-09-15 08:48:40 -0300

    adicionar aula 05

---
+++ Aula-05/modulo/calculoTabuada.js
@@ -0,0 +1,47 @@
+/**********************************************************************************
+ * objetivo: Arquivo de funções para calcular uma tabuada                         *
+ * data:01/09/23                                                                  *
+ * autor: Jessica Pereira dos Santos                                              *
+ * versão:1.0                                                                     *
+ *********************************************************************************/
+
+const calcularTabuada = function (valortabuada, contadorInicial, contadorFinal) {
+    let tabuada = String(valortabuada).replace(',', '.');
+    let contInicial = String(contadorInicial).replace(',', '.');
+    let contFinal = String(contadorFinal).replace(',', '.');
+    let resultado;
+    let status = false;
+    // let valorAuxiliar;
+
+    if (contFinal < contInicial) {
+        // valorAuxiliar = contInicial;
+        // contInicial = contFinal;
+        // contFinal = valorAuxiliar;
+
+        contFinal = contadorInicial;
+        contInicial = contadorFinal;
+    }
+
+    if (tabuada == '' || contInicial == '' || contFinal == '')
+        console.log('ERRO: é obrigatório a entrada de todos os valores');
+    else if (isNaN(tabuada) || isNaN(contInicial) || isNaN(contFinal))
+        console.log('ERRO: é obrigatório a entrada somente números');
+    else {
+        contInicial = Number(contInicial);
+        contFinal = Number(contFinal);
+        tabuada = Number(tabuada);
+
+        // exemplo usando o while
+        while (contInicial <= contFinal) {
+            resultado = tabuada * contInicial;
+            console.log(tabuada + 'x' + contInicial + '=' + resultado);
+            contInicial ++;
+            status = true;
+        }
+    }
+
+    return status;
+
+}
+
+console.log(calcularTabuada(2,0,10));
